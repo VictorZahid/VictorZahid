6f46e24f82987e7f12f1b933ceef444f4e663702
Vic <157188008+VictorZahid@users.noreply.github.com>
2024-04-09 16:20:53 -0600

    MVC
    Modelo vista controlador
    -SESIONES
    -ROLES
    -VALIDACION DE CORREO ELECTRONICO
    
    Signed-off-by: Vic <157188008+VictorZahid@users.noreply.github.com>

---
+++ views/modules/administrador.php
@@ -0,0 +1,60 @@
+<?php
+if( empty(session_id()) && !headers_sent()){
+	session_start();
+}
+
+
+if(!$_SESSION["validar"]){
+
+	echo '<script>window.location:index.php?action=ingresar"</script>';
+
+	exit();
+
+}
+
+  ?>
+
+
+<h1>USUARIOS</h1>
+
+    <table border="1">
+
+    	<thead>
+    		
+    		<tr>
+    			<th>Usuario</th>
+    			<th>Contraseña</th>
+    			<th>Email</th>
+    			<th>Editar</th>
+    			<th>Borrar</th>
+
+    		</tr>
+
+    	</thead>
+
+    	<tbody>
+    		
+    		<?php
+    		$vistaUsuario = new MvcController();
+    		$vistaUsuario -> vistaUsuarioController();
+    		$vistaUsuario -> borrarUsuarioController();
+    		?>
+
+    	</tbody>
+
+
+    	
+    </table>
+
+<?php
+
+if(isset($_GET["action"])){
+
+	if($_GET["action"] == "cambio"){
+
+		echo "Cambio Exitoso";
+
+	}
+}
+
+  ?>
---
+++ views/modules/cliente.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+  ?>
+
+
+<h1>CLIENTE</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, laboriosam ad earum sunt, harum ab, quam iste dolorum<br> excepturi a vitae recusandae fugiat in. Esse nulla alias voluptatibus doloribus? Ab!</p>
+
+    	
+
+<?php
+
+if(isset($_GET["action"])){
+
+	if($_GET["action"] == "cambio"){
+
+		echo "Cambio Exitoso";
+
+	}
+}
+
+  ?>
